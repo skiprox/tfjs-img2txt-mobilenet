f83594aa9349a6135a495c95d497a9e1842e6699
skiprox <seanmichaelscanlan@gmail.com>
2019-04-16 14:18:36 -0400

    Updating index

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const ctx = canvas.getContext('2d');
 
 class Main {
 	constructor() {
-		loadImage('test-images/ss2.jpg').then((image) => {
+		loadImage('test-images/ss3.jpg').then((image) => {
 			ctx.drawImage(image, 0, 0, 1200, 1200);
 		});
 		async function run() {
