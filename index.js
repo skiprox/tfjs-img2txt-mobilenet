cdd5a7fa8bfc1db28348b82e982900c18848a80e
skiprox <seanmichaelscanlan@gmail.com>
2019-04-16 14:00:32 -0400

    Cleaning up

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 'use strict';
 
-require('@tensorflow/tfjs-node');
+require('@tensorflow/tfjs-node-gpu');
 const mobilenet = require('@tensorflow-models/mobilenet');
-const fs = require('fs');
 const { createCanvas, loadImage } = require('canvas');
 const canvas = createCanvas(1200, 1200);
 const ctx = canvas.getContext('2d');
 
 class Main {
 	constructor() {
-		loadImage('test-images/out3.jpg').then((image) => {
+		loadImage('test-images/ss2.jpg').then((image) => {
 			ctx.drawImage(image, 0, 0, 1200, 1200);
-			console.log('what the fuck');
 		});
 		async function run() {
 			const model = await mobilenet.load();
